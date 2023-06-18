51aed842d1ede6b4f417e2394e0969fad2100719
Nguyen Manh Thang <nguyenmanhthang2000.fb@gmail.com>
2023-06-18 12:33:34 +0700

    Initial project

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Data } from "./hello-type";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
